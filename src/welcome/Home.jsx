5313a45e953bbd706b45609f7a6de727354749c0
v4rsh1th <varshithjay6@gmail.com>
2021-11-18 21:31:04 +0530

    more functionality, checkboxes mayhem.

--- src/welcome/Home.jsx
+++ src/welcome/Home.jsx
@@ -20,24 +20,27 @@ export default class Home extends React.Component {
             <input type="text" />
             <label>I'm verified because:</label>
             <div className="reason">
-              <input type="radio" name="verification_reason" id="one_million" checked />
+              <input type="checkbox" name="verification_reason" id="one_million" />
               <label for="one_million">I have a net worth of $1 million, excluding my primary residence</label>
+              <span class="checkmark"></span>
             </div>
             <div className="reason">
-              <input type="radio" name="verification_reason" id="two_hundred"/>
+              <input type="checkbox" name="verification_reason" id="two_hundred"/>
               <label for="two_hundred">I have made over $200K the past two years, and am on track to do so again this year</label>
+              <span class="checkmark"></span>
             </div>
             <div className="reason">
-              <input type="radio" name="verification_reason" id="five_million"/>
-              <label for="five_million">I have a net worth of over $5 million, including illiquid startup stock, i.e. qualifies purchaser</label>
+              <input type="checkbox" name="verification_reason" id="five_million"/>
+              <label for="five_million">I have a net worth of over $5 million, including illiquid startup stock, i.e. qualified purchaser</label>
+              <span class="checkmark"></span>
             </div>
             <label>Supporting documents</label>
             <button>Upload supporting documents</button>
             <label>Email address</label>
             <input type="text" placeholder="name@domain" />
             <label>Payment info</label>
-
             <button type="submit">Submit information for verification</button>
+            <p id="stripe_tag">Powered by <strong>stripe</strong></p>
           </form>
           <div className="footer">
             &copy; 2021 Invaccredition &bull; hi@invaccredition.com
