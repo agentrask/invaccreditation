b915a06a507c510cf99274511ab03529dd7c6ab5
v4rsh1th <varshithjay6@gmail.com>
2021-11-18 16:41:08 +0530

    init

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="header">
+        <h2>Invaccredition</h2>
+        <p>Get a investor accredition in less than 24 hours at <strong>$50</strong></p>
+        <p>A licensed CPA will verify your application.</p>
+      </div>
+      <div className="main">
+
+      </div>
+    </div>
+  );
+}
+
+export default App;
